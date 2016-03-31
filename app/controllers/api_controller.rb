4746793007bfbd6f135fd3d74c7a8da968e63ac9
andela-sdamian <simon.peter@andela.com>
2016-03-31 10:43:21 +0100

    Type: rails-auto-generated Description Code generated by rails automatically

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,25 @@
+class ApiController < ApplicationController
+  
+  def to_short 
+  end
+  
+  def popular_users
+  end
+  
+  def popular_urls
+    
+  end
+  
+  def recently_added
+    
+  end
+  
+  private
+  
+  def set_api_params
+  end
+  
+end
+
+
+
