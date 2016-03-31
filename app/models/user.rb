8a82b94f0d496b08f903c20073ab241213e0404e
andela-sdamian <simon.peter@andela.com>
2016-03-31 01:01:20 +0100

    Type: feature Scope: Devise User Model, & Views Subject: implemented all the functionalities users need to shorten a given url with or without a vanity string Body: implore the devise gem to handle user authentication and protecting certain resouces from site visitors. Added logic and scope to the Link Controller that facilitates this functionality. Also, formatted the view and dashboard that gives user their scope to perfom site functionality. All features here enables a user to shorten a given url when a vanity string is provided or not. [(Finishes #115761583)]

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:twitter],
+         password_length: 6..72
+  
+  validates :full_name, presence: true 
+  has_many :links
+  
+  scope :popular_users, lambda {
+    order('links_count desc').select(:full_name, :created_at, :links_count).
+        limit(3)
+  }
+  
+  
+end
